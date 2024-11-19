96005cabed408cabb78cca665d537cb7551e2534
Youssef <Youssef@MacBook-Air-som-tillhor-Haidar.local>
2024-11-19 11:41:24 +0100

    feat

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, jsonify
 import openai
 
-api_key = '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'
-app = Flask(__name__)
+
 
 @app.route("/prompt")
 def prompt():
